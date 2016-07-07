8420ddd08fc3c33f56d0e04ab4eecd802f7ec22b
Sean Helm <shelm@gmu.edu>
2016-07-06 21:55:22 -0400

    Seperated into header file

--- src/morse/morse.c
+++ src/morse/morse.c
@@ -1,23 +1,7 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <stdint.h>
-
-#define DOT_DELAY 70
-#define DASH_DELAY 300
-#define CHAR_PAUSE_DELAY 300
-#define NEXT_DELAY 300
-#define FINISHED_DELAY 1500
-
-typedef struct morse_code {
-    uint8_t list[4];
-    uint8_t count;
-    char letter;
-} morse_code;
-
-void morse_converter(uint8_t list[], uint8_t size);
-void morse_dot(void);
-void morse_dash(void);
-void delay_ms(uint16_t count);
+#include "morse.h"
 
 int main(void)
 {
